c1e4292fd5fc4942cb5d3da85e614c24505a5ba9
jantapabh <jantapa2407@gmail.com>
2020-02-13 11:40:07 +0700

    commit finish

--- example.js
+++ example.js
@@ -23,6 +23,10 @@ let customer = [
 
     // customer.sort( (a,b) => (b.salary > a.salary) ? -1 : 1 )
 
-       console.log( _.sortBy( customer, 'salary').reverse())
+    //    console.log( _.sortBy( customer, 'salary').reverse())
 
-       console.log(_.slice( customer,1,2))
+    //    console.log(_.slice( customer,1,2))
+
+
+
+    console.log(JSON.stringify(customer));
