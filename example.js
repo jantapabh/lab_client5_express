4e199dda0ebf7eeb1bcda84884f96e86b154c30e
jantapabh <jantapa2407@gmail.com>
2020-02-13 11:31:09 +0700

    test

--- example.js
+++ example.js
@@ -1,13 +1,19 @@
 let customer = [
 
 
-    {name: 'Jan' , salary : 100 },
-    {name: 'Jan2' , salary : 1000 },
-    {name: 'Jan3' , salary : 10000 },
-    {name: 'Jan4' , salary : 100000 },
-    {name: 'Jan5' , salary : 1000000 }
+    { name: 'Jan', salary: 100 },
+    { name: 'Jan2', salary: 1000 },
+    { name: 'Jan3', salary: 10000 },
+    { name: 'Jan4', salary: 100000 },
+    { name: 'Jan5', salary: 1000000 }
 
 ]
 
 
 console.log(customer);
+
+customer.map((item) =>
+    console.log(item.name + '-' + item.salary
+    ))
+
+    customer.sort( (a,b) => (b.salary > a.salary) ? -1 : 1 )
